2d6d61caa591cda4f70f0b2433d2f976377df93d
Rohit Pratti <rohit.pratti@gmail.com>
2019-05-16 00:16:07 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -56,7 +56,7 @@ def for_iterator(number_of_times)
   
   phrase = "Welcome to Flatiron School's Web Development Course!"
   
-  number_of_times = 1..
+  number_of_times = 1..number_of_times.to_i
   
 end
 
